fc9582022217f1f4345f2d21ca003fa50ecf6bff
diefesson <diefesson.so@gmail.com>
2023-06-02 14:56:11 -0300

    Adcionado nomes dos buckets

--- scoop.ps1
+++ scoop.ps1
@@ -1,54 +1,53 @@
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
 
-scoop install aria2
+scoop install main/aria2
 scoop config aria2-enabled false
-scoop install git
+scoop install main/git
 
 scoop bucket add extras
 scoop bucket add games
 scoop bucket add java 
 scoop bucket add sysinternals
 
-scoop install 7zip
-scoop install aimp
-scoop install contig
-scoop install curl
-scoop install deluge
-scoop install dolphin-beta
-scoop install dotnet-sdk
-scoop install exiftool
-scoop install ffmpeg
-scoop install firefox
-scoop install git
-scoop install googlechrome
-scoop install grep
-scoop install imagemagick
-scoop install keepassxc
-scoop install krita
-scoop install less
-scoop install maven
-scoop install megatools
-scoop install msys2
-scoop install nano
-scoop install netcat
-scoop install ngrok
-scoop install nodejs
-scoop install obs-studio
-scoop install pwsh
-scoop install python
-scoop install qemu
-scoop install retroarch
-scoop install rhash
-scoop install rustup
-scoop install shotcut
-scoop install temurin-jdk
-scoop install vlc
-scoop install vncviewer
-scoop install vscode
-scoop install which
-scoop install windows-terminal
-scoop install yt-dlp
+scoop install extras/aimp
+scoop install extras/deluge
+scoop install extras/firefox
+scoop install extras/googlechrome
+scoop install extras/keepassxc
+scoop install extras/krita
+scoop install extras/obs-studio
+scoop install extras/qemu
+scoop install extras/shotcut
+scoop install extras/vlc
+scoop install extras/vncviewer
+scoop install extras/vscode
+scoop install extras/windows-terminal
+scoop install games/dolphin-beta
+scoop install games/retroarch
+scoop install main/7zip
+scoop install main/curl
+scoop install main/dotnet-sdk
+scoop install main/exiftool
+scoop install main/ffmpeg
+scoop install main/grep
+scoop install main/imagemagick
+scoop install main/less
+scoop install main/maven
+scoop install main/megatools
+scoop install main/msys2
+scoop install main/nano
+scoop install main/netcat
+scoop install main/ngrok
+scoop install main/nvm
+scoop install main/pwsh
+scoop install main/python
+scoop install main/rhash
+scoop install main/rustup
+scoop install main/temurin-jdk
+scoop install main/which
+scoop install main/yt-dlp
+scoop install sysinternals/contig
 
 python -m pip install pip_search
 python -m pip install pipenv
