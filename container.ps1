1bcf15b9457671b3efcb873a889bc0bfe69a1b19
diefesson <diefesson.so@gmail.com>
2023-09-16 14:26:48 -0300

    Substituido container.ps1 por podman.ps1

--- container.ps1
+++
@@ -1,5 +0,0 @@
-scoop install main/act
-scoop install main/docker-compose
-scoop install main/podman
-scoop install main/podman-desktop
-
---
+++ podman.ps1
@@ -0,0 +1,4 @@
+scoop install main/act
+scoop install main/podman
+scoop install extras/podman-desktop
+python -m pip install podman-compose
