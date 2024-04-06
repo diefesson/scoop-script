b30baa3aff90344a60442acf751bab6a5a220a95
diefesson <diefesson.so@gmail.com>
2024-04-06 12:32:18 -0300

    Movido rufus para basic.ps1

--- basic.ps1
+++ basic.ps1
@@ -3,4 +3,5 @@ scoop install extras/firefox
 scoop install extras/googlechrome
 scoop install extras/keepassxc
 scoop install extras/localsend
+scoop install extras/rufus
 scoop install extras/vscode
