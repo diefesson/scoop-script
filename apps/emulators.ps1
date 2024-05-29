d8c8aa731508ecc4b5d7f81f38692ec065da85f8
diefesson <diefesson.so@gmail.com>
2024-05-29 19:44:12 -0300

    Added rmg, ryujinx and sudachi to emulators.ps1

--- apps/emulators.ps1
+++ apps/emulators.ps1
@@ -3,6 +3,9 @@ scoop install games/dolphin-beta
 scoop install games/gzdoom
 scoop install games/mame
 scoop install games/pcsx2-dev
+scoop install games/rmg
+scoop install games/ryujinx
+scoop install games/sudachi
 
 $mameroot = scoop prefix mame
 scoop shim add chdman $mameroot\chdman.exe
