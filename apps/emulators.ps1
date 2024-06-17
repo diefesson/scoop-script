19eb15183d27aa162791f225e441838107aae98b
diefesson <diefesson.so@gmail.com>
2024-06-17 18:17:45 -0300

    Added ruffle-nightly to emulators

--- apps/emulators.ps1
+++ apps/emulators.ps1
@@ -5,6 +5,7 @@ scoop install games/pcsx2-dev
 scoop install games/rmg
 scoop install games/ryujinx
 scoop install games/sudachi
+scoop install versions/ruffle-nightly
 
 $mameroot = scoop prefix mame
 scoop shim add chdman $mameroot\chdman.exe
