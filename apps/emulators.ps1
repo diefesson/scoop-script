5824b5fefb9ddf5f1e0d829dee6edc8265ff4dc2
Diefesson <diefesson.so@gmail.com>
2025-10-08 15:24:37 -0300

    Replaced Sudachi with Ryujinx

--- apps/emulators.ps1
+++ apps/emulators.ps1
@@ -3,7 +3,7 @@ scoop install games/dolphin
 scoop install games/mame
 scoop install games/pcsx2-dev
 scoop install games/rmg
-scoop install games/sudachi
+scoop install games/ryujinx
 scoop install versions/ruffle-nightly
 
 $mameroot = scoop prefix mame
