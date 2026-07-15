a75ea5fc69abcab715917a74fea935d1af3bc440
Diefesson <diefesson.so@gmail.com>
2026-07-15 11:01:49 -0300

    Removed chdman shim for games/mame

--- apps/emulation.ps1
+++ apps/emulation.ps1
@@ -8,6 +8,3 @@ scoop install games/ppsspp
 scoop install games/rmg
 scoop install games/ryujinx
 scoop install versions/ruffle-nightly
-
-$mameroot = scoop prefix mame
-scoop shim add chdman $mameroot\chdman.exe
